3d969ef4613fcc4cfc264dfe3aeb27795a53e0be
BenerjiBen <63308610+BenerjiBen@users.noreply.github.com>
2021-04-30 17:32:11 +0530

    Update activity4.h

--- inc/activity4.h
+++ inc/activity4.h
@@ -11,22 +11,14 @@
  * 
  */
 
-/**
- * Macro Definitions
- */
+
 #define F_CPU 16000000UL /**< Clock Frequency of MCU is 16 MHz */
 #define USART_BAUDRATE 9600 /**< Baud rate for serial communication  */
 #define UBRR_VALUE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1) /**< Formula to calculate UBRR value */
 
-/**
- * Include files
- */ 
+
 #include <util/delay.h>
 #include <avr/io.h>
-
-/**
- * Function Definitions
- */
 
 /**
  * @brief Setting Baud rate and Enabling Rx and Tx 
